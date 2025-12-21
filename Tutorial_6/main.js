da9e92b7969672cab60e5b408e454f24edcc9df3
Traxyn <93167062+Traxyn@users.noreply.github.com>
2025-12-20 21:27:57 -0500

    Corrected Typo in Tutorial_6
    main.js, corrected Line 96 to "gl.generateMipmap"

--- Tutorial_6/main.js
+++ Tutorial_6/main.js
@@ -93,7 +93,7 @@ const run = async () => {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 500, 300, 0, gl.RGB, gl.UNSIGNED_BYTE, image);
 
-    gl.generatedMipmap(gl.TEXTURE_2D);
+    gl.generateMipmap(gl.TEXTURE_2D);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
