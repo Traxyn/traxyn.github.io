67b77b340530383b558c8c262aac45b3d9742e72
Traxyn <93167062+Traxyn@users.noreply.github.com>
2025-12-22 17:37:16 -0500

    Starting tutorial 7 (VAOs)
    first push of code for tutorial 7

---
+++ Tutorial_7/main.js
@@ -0,0 +1,68 @@
+const vertexShaderSource = `#version 300 es
+
+layout(location=0) in float aPointSize;
+layout(location=1) in vec4 aPosition;
+layout(location=2) in vec4 aColor;
+
+out vec3 vColor;
+
+void main() {
+    vColor = aColor;
+    gl_PointSize = aPointSize;
+    gl_Position = vec4(aPosition, 0.0, 1.0);
+}`;
+
+const fragmentShaderSource = `#version 300 es
+precision mediump float;
+
+in vec3 vColor;
+out vec4 fragColor;
+
+void main() {
+    fragColor = vColor;
+}`;
+
+const canvas = document.querySelector('canvas').getContext('webgl2');
+
+const program = gl.createProgram();
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vertexShaderSource);
+gl.compileShader(vertexShader);
+gl.attachShader(program, vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, fragmentShaderSource);
+gl.compileShader(fragmentShader);
+gl.attachShader(program, fragmentShader);
+
+gl.linkProgram(program);
+
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.log(gl.getShaderInfoLog(vertexShader));
+    console.log(gl.getShaderInfoLog(fragmentShader));
+    console.log(gl.getShaderInfoLog(program));
+    throw new Error('failed to link');
+}
+
+gl.useProgram(program);
+
+const bufferData = new Float32Array([
+    -.8,.6,     1,.75,.75,      125,
+    -.3,.6,     0,.75,1,        32,
+    .3,.6,      .5,1,.75,       75,
+    .8,.6,      0,.75,.75,      9,
+]);
+const buffer1 = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer1);
+gl.bufferData(gl.ARRAY_BUFFER, data1, gl.STATIC_DRAW);
+
+gl.vertexAttribPointer(0, 1, gl.FLOAT, false, 24, 20);
+gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 24, 0);
+gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 24, 8);
+
+gl.enableVertexAttribArray(0);
+gl.enableVertexAttribArray(1);
+gl.enableVertexAttribArray(2);
+
+gl.drawArrays(gl.POINTS, 0, 4);
