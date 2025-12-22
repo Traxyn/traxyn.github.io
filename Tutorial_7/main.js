64dfdce3785758f6df6fa227e46fa594cc074de3
Traxyn <93167062+Traxyn@users.noreply.github.com>
2025-12-22 17:49:03 -0500

    Tutorial 7 - Corrected another typo

--- Tutorial_7/main.js
+++ Tutorial_7/main.js
@@ -41,7 +41,7 @@ gl.linkProgram(program);
 if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
     console.log(gl.getShaderInfoLog(vertexShader));
     console.log(gl.getShaderInfoLog(fragmentShader));
-    console.log(gl.getShaderInfoLog(program));
+    console.log(gl.getProgramInfoLog(program));
     throw new Error('failed to link');
 }
 
